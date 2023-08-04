dda6ab4ad64ab103e0fd6c961a7d7b8ab137f102
Dmitry Krutkin <krutkin@a2design.biz>
2023-08-04 09:25:13 +0300

    Setting up configs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-function App() {
-  return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  );
-}
+const App = () => <h1 className='text-3xl font-bold'>Hello world!</h1>
 
-export default App;
+export default App
